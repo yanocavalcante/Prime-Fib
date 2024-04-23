5fb50f4784e815d0b3df31901c245cca7b3b843e
Yano Henrique de Melo Ribeiro Cavalcante <yano.melo.cavalcante@gmail.com>
2024-04-23 00:35:30 -0300

    Linear Prime Numbers

--- main.py
+++ main.py
@@ -5,3 +5,6 @@ if __name__ == "__main__":
     #Testing fibonnaci functions
     print(fibonnaci.lin_fib(8))
     print(fibonnaci.rec_fib(8))
+
+    #Testing prime numbers functions
+    print(prime.lin_prime(15))
--- prime.py
+++ prime.py
@@ -1,6 +1,16 @@
 def lin_prime(num: int):
+    primes_list = []
     if num < 1:
         raise Exception
+    elif num == 0 or num == 1:
+        return primes_list
+    for i in range(2, num):
+        for j in range(2,i):
+            if i % j == 0:
+                break
+        else:
+            primes_list.append(i)
+    return primes_list
 
 def rec_prime(num: int):
     if num < 1:
